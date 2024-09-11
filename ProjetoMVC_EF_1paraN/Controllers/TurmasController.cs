d02bf5fd3dba810c30531e83427878adc7cb089e
ricardofrohlich <ricardosma@gmail.com>
2024-09-11 07:40:33 -0300

    Adicionado controller Turmas

---
+++ ProjetoMVC_EF_1paraN/Controllers/TurmasController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC_EF_1paraN.DAL;
+using ProjetoMVC_EF_1paraN.Models;
+
+namespace ProjetoMVC_EF_1paraN.Controllers
+{
+    public class TurmasController : Controller
+    {
+        private readonly Contexto _context;
+
+        public TurmasController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Turmas
+        public async Task<IActionResult> Index()
+        {
+            var contexto = _context.Turmas.Include(t => t.Professor);
+            return View(await contexto.ToListAsync());
+        }
+
+        // GET: Turmas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turmas
+                .Include(t => t.Professor)
+                .FirstOrDefaultAsync(m => m.TurmaId == id);
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            return View(turma);
+        }
+
+        // GET: Turmas/Create
+        public IActionResult Create()
+        {
+            ViewData["ProfessorId"] = new SelectList(_context.Professores, "ProfessorID", "Nome");
+            return View();
+        }
+
+        // POST: Turmas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TurmaId,Nome,ProfessorId")] Turma turma)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turma);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProfessorId"] = new SelectList(_context.Professores, "ProfessorID", "ProfessorID", turma.ProfessorId);
+            return View(turma);
+        }
+
+        // GET: Turmas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turmas.FindAsync(id);
+            if (turma == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProfessorId"] = new SelectList(_context.Professores, "ProfessorID", "ProfessorID", turma.ProfessorId);
+            return View(turma);
+        }
+
+        // POST: Turmas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TurmaId,Nome,ProfessorId")] Turma turma)
+        {
+            if (id != turma.TurmaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(turma);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TurmaExists(turma.TurmaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProfessorId"] = new SelectList(_context.Professores, "ProfessorID", "ProfessorID", turma.ProfessorId);
+            return View(turma);
+        }
+
+        // GET: Turmas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turmas
+                .Include(t => t.Professor)
+                .FirstOrDefaultAsync(m => m.TurmaId == id);
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            return View(turma);
+        }
+
+        // POST: Turmas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var turma = await _context.Turmas.FindAsync(id);
+            if (turma != null)
+            {
+                _context.Turmas.Remove(turma);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TurmaExists(int id)
+        {
+            return _context.Turmas.Any(e => e.TurmaId == id);
+        }
+    }
+}
